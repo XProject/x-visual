a9bcf75d5c449125e99eb2e5b0f652cf74c1673e
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2022-12-24 19:39:50 -0500

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+
+description 'x-fps'
+version '0.0.0'
+
+files {
+    'files/*'
+}
+
+shared_scripts {
+    'shared/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
