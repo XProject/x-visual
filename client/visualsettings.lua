0b0500cfd2ba194af39260b8d7a16390655a0d43
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2022-12-26 01:15:29 -0500

    fix: indentations

--- client/visualsettings.lua
+++ client/visualsettings.lua
@@ -1,15 +1,15 @@
 CreateThread(function()
-	local settingsFile = LoadResourceFile(GetCurrentResourceName(), "files/visualsettings.dat")
-	local lines = stringsplit(settingsFile, "\n")
-	for _, v in ipairs(lines) do
-		if not starts_with(v, '#') and not starts_with(v, '//') and (v ~= "" or v ~= " ") and #v > 1 then
-			v = v:gsub("%s+", " ")
-			local setting = stringsplit(v, " ")
-			if setting[1] and setting[2] and tonumber(setting[2]) ~= nil then
-				if setting[1] ~= 'weather.CycleDuration' then
+    local settingsFile = LoadResourceFile(GetCurrentResourceName(), "files/visualsettings.dat")
+    local lines = stringsplit(settingsFile, "\n")
+    for _, v in ipairs(lines) do
+        if not starts_with(v, '#') and not starts_with(v, '//') and (v ~= "" or v ~= " ") and #v > 1 then
+            v = v:gsub("%s+", " ")
+            local setting = stringsplit(v, " ")
+            if setting[1] and setting[2] and tonumber(setting[2]) ~= nil then
+                if setting[1] ~= 'weather.CycleDuration' then
                     SetVisualSettingFloat(setting[1], tonumber(setting[2]) + 0.0)
-				end
-			end
-		end
-	end
+                end
+            end
+        end
+    end
 end)
--- shared/shared.lua
+++ shared/shared.lua
@@ -17,19 +17,19 @@ if not IsDuplicityVersion() then -- client
 end
 
 function stringsplit(inputString, separator)
-	if not separator then
-		separator = "%s"
-	end
-	local t, i = {}, 1
-	for str in string.gmatch(inputString, "([^"..separator.."]+)") do
-		t[i] = str
-		i = i + 1
-	end
-	return t
+    if not separator then
+        separator = "%s"
+    end
+    local t, i = {}, 1
+    for str in string.gmatch(inputString, "([^"..separator.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
 end
 
 function starts_with(inputString, start)
-	return inputString:sub(1, #start) == start
+    return inputString:sub(1, #start) == start
 end
 
 function dumpTable(table, nb)
